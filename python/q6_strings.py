8f69a5b410de35041918abf17dd981bcb6dfe315
wangjenn <wangjenn@users.noreply.github.com>
2016-09-13 13:27:50 -0700

    Update q6_strings.py

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -137,10 +137,11 @@ def verbing(s):
     """
 
 def not_bad(s):
-
-
-
-
+  n = s.find('not')
+  b = s.find('bad')
+  if n != -1 and b != -1 and b > n:
+    s = s[:n] + 'good' + s[b+3:]
+  return s
 
 
     """
