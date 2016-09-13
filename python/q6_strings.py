51dce19095967b4ce414005cb58bd5cee4e39f8a
wangjenn <wangjenn@users.noreply.github.com>
2016-09-13 13:30:17 -0700

    Update q6_strings.py

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -160,3 +160,10 @@ def not_bad(s):
     'KitDontenut'
     """
  def front_back(a, b):
+     a_middle = len(a)/2 
+     b_middle = len(b)/2 
+     if len (a) % 2 = 1: 
+         a_middle = a_middle + 1 
+     if len (b) % 2 = 1: 
+         b_middle = b_middle + 1 
+     return a[:a_middle] + b[:b_middle] + a[a_middle:] + b[b_middle:]
