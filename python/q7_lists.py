4f39164ad8b873346a013a71d78010fff4056e16
wangjenn <wangjenn@users.noreply.github.com>
2016-09-14 18:05:11 -0700

    Update q7_lists.py

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -106,3 +106,12 @@ def remove_adjacent(nums):
     """
     
 def linear_merge(list1, list2):
+  result = []
+  while len(list1) and len(list2):
+    if list1[0] < list2[0]:
+      result.append(list1.pop(0))
+    else:
+      result.append(list2.pop(0))
+  result.extend(list1)
+  result.extend(list2)
+  return result
